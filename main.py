e1f7ec85e4d475761b30dc9f0c849e16b7ca3301
mahadev aralikatti <mahadevaralikatti2@gmail.com>
2026-09-05 21:09:00 +0530

    simpleprogram

---
+++ main.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+
+def A():
+    print("first function")
+
+def B():
+    print("second function")
+
+if __name__=="__main__":
+    p1=Process(target=A)
+    p2=Process(target=B)
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+    print(" Both process completed")
