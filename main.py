06901e6ac7178374dbd458ae2a5290c43eb21608
mahadev aralikatti <mahadevaralikatti2@gmail.com>
2026-09-08 16:59:21 +0530

    calculation

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def A():
 def B():
     return "second function"
 
+def C(int a,int b):
+    return a+b
+
 if __name__=="__main__":
     p1=Process(target=A)
     p2=Process(target=B)
+    p3=Process(target=C)
     p1.start()
     p2.start()
+    p3.start()
 
     p1.join()
     p2.join()
+    p3.join()
     print(" Both process completed")
--- test_main.py
+++ test_main.py
@@ -4,3 +4,5 @@ def test_A():
     assert A() == "first function"
 def test_B():
     assert B() == "second function"
+def test_C():
+    assert C(1,2) == 3
